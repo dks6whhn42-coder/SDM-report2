f239d931676cc6555a2bd437f2ab89ba75655582
Your Name <you@example.com>
2026-02-05 17:52:27 +0900

    Fix calc() to reject non-integer inputs

--- calc_mul.py
+++ calc_mul.py
@@ -1,26 +1,20 @@
+
 #!/usr/bin/python3
 
 import re
                 
-def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
-        else:
-                valid=False
-                
-        if valid:
-                ans=a*b
-                return ans
-        else:
-                return -1
+def calc(A, B):
+    try:
+        A = int(A)
+        B = int(B)
+    except:
+        return -1
+
+    if 1 <= A <= 999 and 1 <= B <= 999:
+        return A * B
+    else:
+        return -1
+
         
                 
 def main ():
